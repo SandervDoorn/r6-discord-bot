2327ce63afd71693846a88d48f0df4e9baa5f076
Sander van Doorn <s.van.doorn@st.hanze.nl>
2020-02-13 16:24:40 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+import os
+
+import discord as discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+from cogs import teams
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    # ###################################
+    # Load commands
+    print("# Loading commands")
+
+    print("=> Team commands")
+    bot.add_cog(teams.TeamCog(bot))
+
+    print("Done!")
+    print("\n")
+    # ###################################
+
+
+@bot.event
+async def on_guild_join(guild: discord.Guild):
+    print(f'Connected to guild: {guild.name}')
+    print("\n")
+
+    # ###################################
+    # Initializing role management system
+    print("# Verifying management role exists")
+    exists = False
+    for role in guild.roles:
+        if role.name == 'R6 Bot Manager':
+            print("=> Role already exists")
+            exists = True
+
+    if not exists:
+        print("=> Role does not exist")
+        print("# Creating role")
+        await guild.create_role(name="R6 Bot Manager")
+        print("=> Role created")
+    print("\n")
+    # ###################################
+
+
+bot.run(TOKEN)
+
---
+++ cogs/teams.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import requests
+
+
+class TeamCog(commands.Cog):
+
+    @commands.command(name='add_team')
+    async def add_team(self, ctx: commands.Context, teamname: str):
+        reponse = requests.post("http://localhost:8080/add_team", teamname)
+        await ctx.send(f'Team "{teamname}" added!')
+
+    @commands.command(name='remove_team')
+    async def remove_team(self, ctx: commands.Context, teamname: str):
+        response = requests.post("http://localhost:8080/remove_team", teamname)
+        await ctx.send(f'Team "{teamname}" has been removed')
+
+    @commands.command(name='rename_team')
+    async def rename_team(self, ctx: commands.Context, from_name: str, to_name: str):
+        requests.post("http://localhost:8080/rename_team", from_name, to_name)
+        await ctx.send(f'Team "{from_name}" has been renamed to: "{to_name}"')
+
+    @commands.command(name='join_team')
+    async def join_team(self, ctx: commands.Context, teamname: str):
+        # TODO: Get team from backend and put it into dto
+        # TODO: Verify the user is not part of the team already
+        requests.post("http://localhost:8080/join_team", teamname, ctx.author.name)
+        await ctx.send(f'{ctx.author.name} has just joined team "{teamname}"')
