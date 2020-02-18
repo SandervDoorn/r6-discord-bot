f7918a7ff9c88b5e8c5c9cf9553cf42e49ca10ea
Sander van Doorn <s.van.doorn@st.hanze.nl>
2020-02-18 15:28:26 +0100

    Add spaces in feedback string

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -6,6 +6,6 @@ class MiscellaneousCog(commands.Cog):
     @commands.command("feedback")
     async def feedback(self, ctx: commands.Context):
         await ctx.send("Feedback is more than welcome!\nYou can checkout the GitHub page of this project here: "
-                 "https://github.com/SandervDoorn/r6-discord-bot\n\nFor any ideas or bug reports you can message me"
-                 "directly on discord: VersionTwo#2528 or leave a message on GitHub\n\nYour help is very much"
-                 "appreciated!")
+                       "https://github.com/SandervDoorn/r6-discord-bot\n\nFor any ideas or bug reports you can message "
+                       "me directly on discord: VersionTwo#2528 or leave a message on GitHub\n\nYour help is very much "
+                       "appreciated!")
