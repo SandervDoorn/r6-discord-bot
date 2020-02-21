47999abbafa54da92973331ce1deccf934f8d624
Sander van Doorn <s.van.doorn@st.hanze.nl>
2020-02-21 11:45:01 +0100

    Captain can now kick players

--- cogs/captaincommands.py
+++ cogs/captaincommands.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 
 from database.base import session_factory
-from database.repository import find_player_by_discord_id, add_player_to_team
+from database.repository import find_player_by_discord_id, add_player_to_team, find_team_by_name
 from validation.permissions import is_captain
+from errors.exceptions import UserNotInTeamError
 
 
 class CaptainCommands(commands.Cog):
@@ -20,3 +21,16 @@ class CaptainCommands(commands.Cog):
         session.close()
         add_player_to_team(teamname, p)
         await ctx.send(f"{p.name} is now part of team {captain.team.name}!")
+
+    @commands.command("kick")
+    @is_captain()
+    async def kick(self, ctx: commands.Context, player_mention):
+        p = ctx.message.mentions[0]
+        session = session_factory()
+        captain = find_player_by_discord_id(ctx.author.id, session)
+        player = find_player_by_discord_id(p.id, session)
+        if player in captain.team.players:
+            captain.team.players.remove(player)
+        else:
+            raise UserNotInTeamError
+        await ctx.message(f"{p.name} has been kicked from the team!")
